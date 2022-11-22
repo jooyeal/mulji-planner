2ac0e8bb8232819ee4b2e0b20fb298b7da38b164
車 周悦 <jyol1234@gmail.com>
2022-11-22 18:25:28 +0900

    add reload button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import { Plan } from "@prisma/client";
 import axios from "axios";
 import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import PlanComponent from "../components/Plan";
+import { IoIosRefresh } from "react-icons/io";
 
 type Props = {
   data: {
@@ -12,6 +15,7 @@ type Props = {
 };
 
 export const Home: React.FC<Props> = ({ data: { status, plans, message } }) => {
+  const router = useRouter();
   if (status !== 200) {
     return (
       <div className="flex justify-center items-center">
@@ -21,24 +25,28 @@ export const Home: React.FC<Props> = ({ data: { status, plans, message } }) => {
   }
   return (
     <div className="flex flex-col items-center">
-      <div className="w-full">
+      <div className="w-full flex justify-between items-center">
         <p className="text-2xl">현재일정</p>
+        <IoIosRefresh size={25} onClick={() => router.reload()} />
       </div>
+      <br />
       <div className="flex flex-col items-center w-full gap-2">
-        {plans?.map((plan) => (
-          <PlanComponent
-            key={plan.id}
-            id={plan.id}
-            title={plan.title}
-            desc={plan.desc}
-            manager={plan.manager}
-            participants={plan.participants}
-            budget={plan.budget}
-            startDate={plan.startDate}
-            endDate={plan.endDate}
-            isOver={plan.isOver}
-          />
-        ))}
+        {plans?.length === 0 && <p>현재 일정이 없습니다</p>}
+        {plans?.length !== 0 &&
+          plans?.map((plan) => (
+            <PlanComponent
+              key={plan.id}
+              id={plan.id}
+              title={plan.title}
+              desc={plan.desc}
+              manager={plan.manager}
+              participants={plan.participants}
+              budget={plan.budget}
+              startDate={plan.startDate}
+              endDate={plan.endDate}
+              isOver={plan.isOver}
+            />
+          ))}
       </div>
     </div>
   );
