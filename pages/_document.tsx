b89b29131e0c47ec856056d33562d02a0a7906a6
車 周悦 <jyol1234@gmail.com>
2022-11-26 19:08:05 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
       <Head />
       <Head>
         <title>물지플래너</title>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="icon"
