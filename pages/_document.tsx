55437a16b7d82d812ee075505de45e84de7d0600
車 周悦 <jyol1234@gmail.com>
2022-11-26 19:16:14 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-touch-fullscreen" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="icon"
