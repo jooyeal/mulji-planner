4140fea606334f153cf1ba32c15d813f1a2542c3
車 周悦 <cha_j@mti.co.jp>
2022-12-14 13:22:21 +0900

    birthday check

--- pages/api/mail/cron.ts
+++ pages/api/mail/cron.ts
@@ -2,7 +2,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 
-const birthdays = [];
+const birthdays = [
+  { day: "12-29", name: "주열" },
+  { day: "12-14", name: "테스트" },
+  { day: "12-15", name: "테스트" },
+];
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,21 +23,26 @@ export default async function handler(
     },
   });
 
-  transporter.sendMail(
-    {
-      from: HOST_EMAIL,
-      to: HOST_EMAIL,
-      subject: "생일 축하 합니다~~~~~~",
-      text: "sending mail by nodemailer",
-    },
-    function (error, info) {
-      if (error) {
-        console.error(error);
-        return res.status(500).json("mail send error");
-      } else {
-        console.log(`Email sent: ${info.response}`);
-        return res.status(200).json("mail send success");
-      }
+  const today = `${new Date().getMonth() + 1}-${new Date().getDate()}`;
+  birthdays.forEach((bd) => {
+    if (bd.day === today) {
+      transporter.sendMail(
+        {
+          from: HOST_EMAIL,
+          to: HOST_EMAIL,
+          subject: `${bd.name} 생일 축하 합니다~~~~~~`,
+          text: `${bd.name}이가 오늘 생일입니다잉~`,
+        },
+        function (error, info) {
+          if (error) {
+            console.error(error);
+            return res.status(500).json("mail send error");
+          } else {
+            console.log(`Email sent: ${info.response}`);
+            return res.status(200).json("mail send success");
+          }
+        }
+      );
     }
-  );
+  });
 }
