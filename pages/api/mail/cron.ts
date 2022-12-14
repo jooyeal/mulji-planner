9f990b35a27e25fe3d6f18f28a002a99c0e892ed
車 周悦 <cha_j@mti.co.jp>
2022-12-14 15:21:59 +0900

    test data

--- pages/api/mail/cron.ts
+++ pages/api/mail/cron.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 
 const birthdays = [
+  { day: "12-14", name: "테스트" },
   { day: "12-15", name: "테스트" },
   { day: "3-28", name: "대현" },
   { day: "4-6", name: "건" },
