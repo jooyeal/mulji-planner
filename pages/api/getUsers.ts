94cee9521327cbebc4f0a99e2d1762eb7464a09c
jooyeal <34053954+jooyeal@users.noreply.github.com>
2022-12-26 17:27:35 +0900

    getUsers

---
+++ pages/api/getUsers.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma, { User } from "../../utils/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ users?: User[]; message: string }>
+) {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json({ users, message: "성공" });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "에러가 발생했습니다. 관리자에게 문의해주세요." });
+  }
+}
